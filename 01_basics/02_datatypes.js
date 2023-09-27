dac5f30f93fe4e31f18f28e8b8d81faeb901e5f7
Nupurpatwal <137989160+NupurPatwal@users.noreply.github.com>
2023-09-27 14:12:52 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // use all code as the newerversion of javascript
+
+// alert(8 + 4) // we are currently using node not browser.
+console.log(9+3) 
+
+console.log("hello world")
+// take away the code should be readable and the indentation should be properly done.
+/*
+1. number => 2 **52 or 53
+2. string => ""
+3. boolean=> true/false
+4.bigint => for using a biggg number not use majorly 
+5. undefined => the value is not defined by js / oR the value is not defined yet
+6. null=> the value is not defined by the user aka standalone value/ or  no value
+7. symbol=> talk about uniqueness.
+ */
+let name ="Nupur Patwal"
+let num= 5
+let val= true 
+
+console.log(typeof num)
+console.log(typeof name)
+console.log(typeof val)
+
+console.log(typeof undefined) // undefined bc it is a type
+console.log(typeof null) // object
