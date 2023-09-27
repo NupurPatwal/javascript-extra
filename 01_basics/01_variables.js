288c2fea827b4fa62b27c5dd7cc67753c3146563
Nupurpatwal <137989160+NupurPatwal@users.noreply.github.com>
2023-09-27 13:38:08 +0000

    declaring var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =12345
+let name ="Nupur"
+var  email= "nupurpatwall@gmail.com"
+ city ="newDelhi"
+
+ /*please not use scope because of issues in functional scope and var scope */
+
+  email = "nuput@outlook.com"
+  name = "rupun"
+  city= "dehradun"
+
+ console.log(name);
+console.table([email,name,city,accountId])
+
