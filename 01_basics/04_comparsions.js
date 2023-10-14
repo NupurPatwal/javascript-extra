65a887f9f1c7569a92d4386843585fad9a43414a
Nupurpatwal <137989160+NupurPatwal@users.noreply.github.com>
2023-10-14 10:17:13 +0000

    comparsion in javascript

---
+++ 01_basics/04_comparsions.js
@@ -0,0 +1,13 @@
+// ****************COMPARSIONS IN OPERATORS *********************
+
+// console.log(2>1);
+// console.log(1>=2);
+// console.log(2!=1);
+// console.log(2==2);
+// console.log(1<=2);
+
+console.log(undefined ==0);
+console.log(null>0);
+console.log(null>=0);
+console.log("2"==2);
+console.log("2"===2); // this is known as strict check ..checks both value and datatype.
