86a586396078c10ef3525c4e9daee0457a88f8e8
Nupurpatwal <137989160+NupurPatwal@users.noreply.github.com>
2023-10-14 10:00:14 +0000

    operations

---
+++ 01_basics/03_part2Operations.js
@@ -0,0 +1,17 @@
+// ***************OPERATIONBS***************
+let x=7;
+let negvalue= -x;
+// console.log("the value of negvalue is" + negvalue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2%3);
+// console.log(2/3);
+console.log(2**3);
+
+console.log(1+"2");
+console.log("1"+2);
+console.log(1+"2" + 3) ;
+console.log("1" + 2+2);
+console.log(1+2 + "2");
