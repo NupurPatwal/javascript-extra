096457e8ed48877f5113dae4168a4656fc399748
Nupurpatwal <137989160+NupurPatwal@users.noreply.github.com>
2023-10-14 13:10:10 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,26 @@
+const x=400;
+// console.log(x);
+
+const y= new Number(300);
+// console.log(y);
+// console.log(y.toFixed(3));
+
+
+const z= 123.199;
+// console.log(z.toPrecision(3));
+
+const hund=100000000;
+// console.log(hund.toLocaleString('en-IN'));
+
+//++++++++++++++ MATHS +++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-3.3));
+// console.log(Math.round(6.2));
+// console.log(Math.ceil(3.2));
+// console.log(Math.floor(3.2));
+// console.log(Math.pow(2,4));
+// console.log(Math.min(6,54,432,2321,54,2,9));
+// console.log(Math.max(6,54,432,2321,54,2,9));
+
+console.log(Math.random());
+console.log( Math.floor(Math.random() * 10) + 1);
