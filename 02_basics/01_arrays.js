5c50ef7bbf90c9632b342266d5c8579ed6cb7bf7
Nupurpatwal <137989160+NupurPatwal@users.noreply.github.com>
2023-10-17 10:31:38 +0000

    Array part 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,9 @@
+//array
+let placed1= ["  Microsoft" , "  Amazon" , "  Google", "  Netflix" , "  Atlassian" , "  Meta"];
+let placed2 =[" MCkensey" , "Apple " , "Goldmansachs " , "Nutanix  " , "Adobe  " ,"Mcercedes  "];
+console.log(`the value of first prefrence is ${placed1}`);
+console.log(`The value of second orefrence is ${placed2}`);
+
+//methods in array ;
+placed1.push("UBER");
+console.log(typeof placed1);
